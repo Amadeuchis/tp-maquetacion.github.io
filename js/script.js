091315eefb1f308be63e2217980ede694198c619
Amadeo Chaves <amacasla@gmail.com>
2025-05-20 14:29:26 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleBtn = document.getElementById('menu-toggle');
-    const nav = document.querySelector('nav');
-  
-    toggleBtn.addEventListener('click', function () {
-      nav.classList.toggle('active');
-    });
-  });
